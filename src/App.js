1407c388e1d80b7ea237007bdebac32339228d15
l.mclennan <l.mclennan@LEWISMCLENNANDE>
2019-07-31 09:36:19 +0100

    json Placeholder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,20 +7,20 @@ class App extends Component {
     super();
 
     this.state = {
-        name: 'Lewis'
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({ monsters: users }))
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello {this.state.name}!
-        </p>
-            <button onClick={() => this.setState({ name: 'Lisa' })}>Change Name</button>
-        </header>
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
+        }
       </div>
     );
   }
